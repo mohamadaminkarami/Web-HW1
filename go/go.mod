1462977ff44df3ce31934f62c49d47e659efae78
moroukian <moroukianali@gmail.com>
2021-10-18 15:14:46 +0330

    add API to get from/set to redis

--- go/go.mod
+++ go/go.mod
@@ -2,4 +2,7 @@ module github.com/mohamadaminkarami/Web-HW1
 
 go 1.13
 
-require github.com/go-redis/redis/v8 v8.11.4
+require (
+	github.com/gin-gonic/gin v1.7.4
+	github.com/go-redis/redis/v8 v8.11.4
+)
--- go/main.go
+++ go/main.go
@@ -2,15 +2,66 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v8"
 )
 
 var ctx = context.Background()
 var rdb = initRedis()
 
 func main() {
-	setValueRedis("key", "value")
+	router := gin.Default()
+	router.GET("/sha", getHash)
+	router.POST("/sha", setHash)
 
-	var value, _ = getValueRedis("key")
-	fmt.Println(value)
+	router.Run("localhost:8080")
+}
+
+func getHash(c *gin.Context) {
+	key := c.Query("key")
+
+	if key == "" {
+		c.JSON(http.StatusBadRequest, map[string]string{
+			"detail": "key param is required",
+		})
+		return
+	}
+
+	value, err := getValueRedis(key)
+
+	switch {
+	case err == redis.Nil:
+		c.JSON(http.StatusNotFound, gin.H{
+			"detail": "key not found",
+		})
+		return
+	case err != nil:
+		panic(err)
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"value": value,
+	})
+}
+
+func setHash(c *gin.Context) {
+	key := c.PostForm("key")
+	value := c.PostForm("value")
+
+	if key == "" || value == "" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"detail": "key & value are required",
+		})
+		return
+	}
+
+	err := setValueRedis(key, value)
+	if err != nil {
+		panic(err)
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"detail": key + " set to " + value,
+	})
 }
