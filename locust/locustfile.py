520343dec320315199f0ff02de36abab03ea7513
moroukian <moroukianali@gmail.com>
2021-10-24 13:20:10 +0330

    fix locust urls

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -21,21 +21,21 @@ class CustomUser(HttpUser):
     @task
     def get_sha_node(self):
         encoded = hash_string(get_random_string())
-        self.client.get(f"/node/sha?encoded={encoded}")
+        self.client.get(f"/node/sha256/?encoded={encoded}")
 
     @task(10)
     def set_sha_node(self):
         decoded = get_random_string()
         data = {"raw_string": decoded}
-        self.client.post("/node/sha", json=data)
+        self.client.post("/node/sha256/", json=data)
 
     @task
     def get_sha_go(self):
         encoded = hash_string(get_random_string())
-        self.client.get(f"/go/sha?encoded={encoded}")
+        self.client.get(f"/go/sha256/?encoded={encoded}")
 
     @task(10)
     def set_sha_go(self):
         decoded = get_random_string()
         data = {"raw_string": decoded}
-        self.client.post("/go/sha", json=data)
+        self.client.post("/go/sha256/", json=data)
